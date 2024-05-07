2a152d8d637ff6a267a6d1d44a8f0c4aea25c226
Ranga Sai <19BQ1A04C8@vvit.net>
2024-05-07 16:38:12 +0530

    Sequence_of_Letters_task

---
+++ Assignments/SequenceOfLetters/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Program That Prints Sequence of letters between string of two Letters 
+ * 
+ */
+using System.Text;
+
+namespace SequenceOfLetters
+{
+    internal class Program
+    {
+        //Method That retuen the string of letters that are between the Two letters
+        public static string SequenceBetweenLetters(string str)
+        {
+            StringBuilder sb = new StringBuilder(); 
+           
+                for (int i = (int)str[0]; i <= (int)str[1]; i++)
+                {
+                    sb.Append((char)i + " ");
+                }
+                return sb.ToString();
+           
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String of two Letters:");
+            string lettersString = Console.ReadLine();
+           
+            if((int)lettersString[0] < (int)lettersString[1])
+            {
+                Console.WriteLine(SequenceBetweenLetters(lettersString));
+            }
+            else
+            {
+                Console.WriteLine("fisrt letter should be before second letter ");
+            }
+
+
+        }
+    }
+}
