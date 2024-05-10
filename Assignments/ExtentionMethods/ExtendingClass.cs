f50f700966926b78fe6c0d5bc5aafaa7c991ad97
Ranga Sai <19BQ1A04C8@vvit.net>
2024-05-10 15:37:52 +0530

    extention methods

---
+++ Assignments/ExtentionMethods/ExtendingClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethods;
+
+internal sealed class ExtendingClass
+{
+    public int Addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+}
---
+++ Assignments/ExtentionMethods/ExtentionClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethods;
+
+internal static class ExtentionClass
+{
+    //Extention Methods for the user defined type ExtendingClass
+    public static int Mul(this ExtendingClass extentionClass, int a, int b)
+    {
+
+        return a * b;
+    }
+
+    public static int Div(this ExtendingClass ec, int a, int b)
+    {
+        return a / b;
+    }
+    public static int PowerOfNumber(this ExtendingClass ec, int baseValue, int exponentValue)
+    {
+        if (exponentValue == 0)
+        {
+            return 1;
+        }
+        else if (exponentValue == 1)
+        {
+            return baseValue;
+        }
+        else
+        {
+            return baseValue * ec.PowerOfNumber(baseValue, exponentValue - 1);
+        }
+    }
+
+    //Extending a  Method  for a Pre-defined Structure
+    public static int Factorial(this Int32 value)
+    {
+        if (value == 1 || value == 0)
+        {
+            return 1;
+        }
+        else if (value == 2)
+        {
+            return 2;
+        }
+        else
+        {
+            return value * Factorial(value - 1);
+        }
+    }
+
+    //Extending  Method for In-Built class  
+    public static string ToProperCase(this String OldString)
+    {
+        StringBuilder NewString = new StringBuilder();
+        if (!String.IsNullOrEmpty(OldString))
+        {
+            OldString = OldString.ToLower();
+            string[] Words = OldString.Split(' ');
+
+            foreach (string W in Words)
+            {
+                char[] chars = W.ToCharArray();
+                chars[0] = char.ToUpper(chars[0]);
+                NewString.Append(chars);
+                NewString.Append(" ");
+            }
+            return NewString.ToString();
+        }
+        return OldString;
+    }
+}
