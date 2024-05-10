e875ee173594bbb019e44f3dd40baf23fd548cd4
Ranga Sai <19BQ1A04C8@vvit.net>
2024-05-10 16:18:20 +0530

    dictionary  program

---
+++ Assignments/DictionaryPractice/Operations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPractice;
+
+internal class Operations
+{
+    private static int InitialId = 101;
+    public Dictionary<int, Student> Students = new Dictionary<int, Student>()
+    {
+        { InitialId,new Student{Name = "Bala",Standard = "Fifth",Section = 'C',ContactNumber = 987987} }
+    };
+    public void AddStudent(Student student)
+    {
+        Students.Add(++InitialId, student);
+    }
+    public void RemoveStudentUsingKey(int key)
+    {
+        Students.Remove(key);
+    }
+    public void UpdateStudent(int Id, Student student)
+    {
+        Students[Id] = student;
+    }
+    public string DisplayStudents()
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (int key in Students.Keys)
+        {
+            sb.AppendFormat("Id = {0}{1}", key, Environment.NewLine);
+            sb.AppendFormat("Student Name = {0}{1}", Students[key].Name, Environment.NewLine);
+            sb.AppendFormat("Student Standard = {0}{1}", Students[key].Standard, Environment.NewLine);
+            sb.AppendFormat("Student Section = {0}{1}", Students[key].Section, Environment.NewLine);
+            sb.AppendFormat("Student ContactNumber = {0}{1}", Students[key].ContactNumber, Environment.NewLine);
+
+        }
+        return sb.ToString();
+    }
+}
---
+++ Assignments/DictionaryPractice/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPractice;
+
+internal class Student
+{
+    public string? Name { get; set; }
+
+    public string? Standard { get; set; }
+    public char Section { get; set; }
+    public int ContactNumber { get; set; }
+}
